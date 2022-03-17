cbf64ac635bb2bb22464c3903589b73aec7944e9
NGendre <noe.gendre@outlook.fr>
2022-03-17 11:46:10 +0100

    exercices

--- src/test/java/java8/ex01/Lambda_01_Test.java
+++ src/test/java/java8/ex01/Lambda_01_Test.java
@@ -2,6 +2,8 @@ package java8.ex01;
 
 import java8.data.Data;
 import java8.data.Person;
+
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -37,9 +39,15 @@ public class Lambda_01_Test {
     public void test_filter_by_age() throws Exception {
 
         List<Person> personList = Data.buildPersonList(100);
-
+        PersonPredicate personPredicate = p -> {
+        	if (p.getAge()>= 18) {
+				return true;
+			} else {
+				return false;
+			}};
+        
         // TODO result ne doit contenir que des personnes adultes (age >= 18)
-        List<Person> result = filter(personList, null);
+        List<Person> result = filter(personList, personPredicate);
 
         assert result.size() == 83;
 
@@ -54,9 +62,10 @@ public class Lambda_01_Test {
     public void test_filter_by_firstname() throws Exception {
 
         List<Person> personList = Data.buildPersonList(100);
-
+       
+        
         // TODO result ne doit contenir que des personnes dont le prénom est "first_10"
-        List<Person> result = filter(personList, null);
+        List<Person> result = filter(personList, p->p.getFirstname().equals("first_10"));
 
         assert result.size() == 1;
         assert result.get(0).getFirstname().equals("first_10");
@@ -74,7 +83,7 @@ public class Lambda_01_Test {
 
         // TODO result ne doit contenir que les personnes dont l'age est > 49 et dont le hash du mot de passe correspond à la valeur de la variable passwordSha512Hex
         // TODO Pour obtenir le hash d'un mot, utiliser la méthode DigestUtils.sha512Hex(mot)
-        List<Person> result = filter(personList, null);
+        List<Person> result = filter(personList, p->p.getAge()>49 && DigestUtils.sha512Hex(p.getPassword()).equals(passwordSha512Hex));
 
         assert result.size() == 6;
         for (Person person : result) {
